5dfb173650475124c5e0572aff114db0deda35bf
Neil Puttick <m@idst.org>
2018-12-26 17:01:52 +0000

    added events

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import Layout from '../components/Layout';
 import Landing from '../sections/Landing';
 import About from '../sections/About';
 import Projects from '../sections/Projects';
+import Events from '../sections/Events';
 import Writing from '../sections/Writing';
 
 const IndexPage = () => (
@@ -10,6 +11,7 @@ const IndexPage = () => (
     <Landing />
     <About />
     <Projects />
+    <Events />
     <Writing />
   </Layout>
 );
